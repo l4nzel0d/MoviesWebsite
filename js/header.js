ff3fe46ce69ab807e8bb9f777bbec6b295936550
l4nzel0d <borisych095@yandex.ru>
2024-11-17 14:49:56 +0300

    feat: add _header styling

---
+++ js/header.js
@@ -0,0 +1,10 @@
+// JavaScript to load header into the DOM
+document.addEventListener("DOMContentLoaded", function () {
+    fetch('components/_header.html')  // Path to the header HTML component
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById('header').innerHTML = data; // Inject into the DOM
+      })
+      .catch(error => console.error('Error loading header:', error));
+  });
+  
