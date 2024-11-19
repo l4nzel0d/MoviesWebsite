560a6dd4d4ec7ab466d6039fc6996a6e1297664e
l4nzel0d <borisych095@yandex.ru>
2024-11-19 22:22:02 +0300

    fix: try to fix slider item configuration breaking

--- js/celebrities.js
+++ js/celebrities.js
@@ -1,46 +1,55 @@
 $(document).ready(function () {
-    $(".lightSlider").lightSlider({
-        loop: true,
-        pager: false,
-        item: 6,
-        responsive: [
-            {
-                breakpoint: 2200,
-                setting: {
-                    item: 6,
+    function initializeLightSlider() {
+        $(".lightSlider").lightSlider({
+            loop: true,
+            pager: false,
+            item: 6,
+            responsive: [
+                {
+                    breakpoint: 2200,
+                    settings: {
+                        item: 6,
+                    },
                 },
-            },
-            {
-                breakpoint: 1400,
-                settings: {
-                    item: 6,
+                {
+                    breakpoint: 1400,
+                    settings: {
+                        item: 6,
+                    },
                 },
-            },
-            {
-                breakpoint: 1242,
-                settings: {
-                    item: 5,
+                {
+                    breakpoint: 1242,
+                    settings: {
+                        item: 5,
+                    },
                 },
-            },
-            {
-                breakpoint: 992,
-                settings: {
-                    item: 4,
+                {
+                    breakpoint: 992,
+                    settings: {
+                        item: 4,
+                    },
                 },
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    item: 3,
+                {
+                    breakpoint: 768,
+                    settings: {
+                        item: 3,
+                    },
                 },
-            },
-            {
-                breakpoint: 576,
-                settings: {
-                    item: 2,
+                {
+                    breakpoint: 576,
+                    settings: {
+                        item: 2,
+                    },
                 },
-            },
-        ],
+            ],
+        });
+    }
+
+    // Initialize lightSlider on page load
+    initializeLightSlider();
+
+    // Listen for fullscreen changes
+    $(document).on("fullscreenchange webkitfullscreenchange mozfullscreenchange MSFullscreenChange", function () {
+        initializeLightSlider(); // Reinitialize lightSlider when fullscreen is toggled
     });
 });
-
