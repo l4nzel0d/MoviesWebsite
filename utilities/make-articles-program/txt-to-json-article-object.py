c7eab6c8d389eeadf207cc5218e84b89c9bdb04b
l4nzel0d <borisych095@yandex.ru>
2024-11-19 15:45:57 +0300

    tweaks: tweaks related to text

--- utilities/make-articles-program/txt-to-json-article-object.py
+++ utilities/make-articles-program/txt-to-json-article-object.py
@@ -1,5 +1,15 @@
 import sys
 import json
+import unicodedata
+
+def normalize_text(text):
+    return text
+    return (unicodedata.normalize('NFKD', text)
+        .replace('—', '-')
+        .replace('‘', "'")
+        .replace('’', "'")
+        .replace('“', '"')
+        .replace('”', '"')) 
 
 def main():
     if len(sys.argv) != 2:
@@ -12,14 +22,17 @@ def main():
     img_path = f"data/articles/{article_name}-Article-Image.jpg"
 
     try:
-        with open(input_file_name, 'r') as input_file:
+        with open(input_file_name, 'r', encoding="utf-8") as input_file:
             content = input_file.readlines()
     except FileNotFoundError:
         print(f"Error: File '{input_file_name}' not found.")
         sys.exit(1)
+    except UnicodeDecodeError:
+        print("Error: Unable to decode the input file. Ensure it is UTF-8 encoded.")
+        sys.exit(1)
     
-    headline = content[0].strip()
-    paragraphs = [p.strip().replace('\n', '').replace('"', '\"')
+    headline = normalize_text(content[0].strip())
+    paragraphs = [normalize_text(p.strip()).replace('\n', '').replace('"', '\"')
                    for p in ''.join(content[1:]).split("\n\n") if p.strip()]
 
     formatted_paragraphs = [f"<p>{paragraph}</p>" for paragraph in paragraphs]
