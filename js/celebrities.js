428274bbd1467f9d8ccc0b7f6611448ccfda3128
l4nzel0d <borisych095@yandex.ru>
2024-11-19 22:14:36 +0300

    feat: add second slider

--- js/celebrities.js
+++ js/celebrities.js
@@ -1,3 +1,46 @@
 $(document).ready(function () {
-    $("#lightSlider").lightSlider();
+    $(".lightSlider").lightSlider({
+        loop: true,
+        pager: false,
+        item: 6,
+        responsive: [
+            {
+                breakpoint: 2200,
+                setting: {
+                    item: 6,
+                },
+            },
+            {
+                breakpoint: 1400,
+                settings: {
+                    item: 6,
+                },
+            },
+            {
+                breakpoint: 1242,
+                settings: {
+                    item: 5,
+                },
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    item: 4,
+                },
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    item: 3,
+                },
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    item: 2,
+                },
+            },
+        ],
+    });
 });
+
