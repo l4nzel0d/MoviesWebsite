9ddfb30ffc768d0397160dffb533f4c8c3e92fad
l4nzel0d <borisych095@yandex.ru>
2024-11-20 22:56:39 +0300

    feat: add second page; tweak: tweak position of sign in box on smaller devices

--- js/login.js
+++ js/login.js
@@ -7,6 +7,26 @@ form.addEventListener("submit", (e) => {
     if (!form.checkValidity()) {
         e.preventDefault();
     }
+});
 
-    emailAndPasswordSection.classList.add("was-validated");
+document.addEventListener("DOMContentLoaded", () => {
+    const formMainPage = document.getElementById("form-main-page");
+    const mainPageButtons = formMainPage.querySelectorAll(
+        ".sign-in-form-button"
+    );
+
+    const formSecondPage = document.getElementById("form-second-page");
+    const goBackButton = document.getElementById("go-back-button");
+
+    goBackButton.addEventListener("click", () => {
+        formSecondPage.style.display = "none";
+        formMainPage.style.display = "block";
+    });
+
+    mainPageButtons.forEach((button) =>
+        button.addEventListener("click", () => {
+            formMainPage.style.display = "none";
+            formSecondPage.style.display = "flex";
+        })
+    );
 });
