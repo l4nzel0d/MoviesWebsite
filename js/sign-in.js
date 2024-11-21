b119fa5f8238e3ed0234024e2381bbc82b014022
l4nzel0d <borisych095@yandex.ru>
2024-11-21 15:28:23 +0300

    fix: make form container height dynamic based on the main-page dimensions, tweak form alignment along y-axis

---
+++ js/sign-in.js
@@ -0,0 +1,36 @@
+const initializeButtons = () => {
+    const formMainPage = document.getElementById("form-main-page");
+    const formSecondPage = document.getElementById("form-second-page");
+    const mainPageButtons = formMainPage.querySelectorAll(".sign-in-form-button");
+    const goBackButton = document.getElementById("go-back-button");
+
+    // Function to toggle visibility of forms
+    const showPage = (pageToShow, pageToHide) => {
+        pageToShow.classList.remove("is-hidden");
+        pageToShow.classList.add("is-visible");
+
+        pageToHide.classList.remove("is-visible");
+        pageToHide.classList.add("is-hidden");
+    };
+
+    goBackButton.addEventListener("click", () => {
+        showPage(formMainPage, formSecondPage);
+    });
+
+    mainPageButtons.forEach((button) => {
+        button.addEventListener("click", () => {
+            showPage(formSecondPage, formMainPage);
+        });
+    });
+};
+
+window.addEventListener("focusout", function () {
+    if (window.innerHeight < document.documentElement.clientHeight) {
+        window.scrollTo(0, 0);
+    }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    // Initialize buttons for navigation
+    initializeButtons();
+});
