c1ba7840dc633f4ac4f79955d2813b568cac6379
l4nzel0d <borisych095@yandex.ru>
2024-11-21 14:10:39 +0300

    refactor: refactor bootstrap import

--- js/header.js
+++ js/header.js
@@ -1,37 +1,40 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Create a new <header> element
+    const bootstrapScriptId = "importBootstrap";
+    const headElement = document.head;
+
+    if (!document.getElementById(bootstrapScriptId)) {
+        const bootstrapScript = document.createElement("script");
+        bootstrapScript.id = bootstrapScriptId; 
+        bootstrapScript.defer = true; 
+        bootstrapScript.src = "js/importBootstrap.js"; 
+        headElement.appendChild(bootstrapScript);
+    }
+
     const headerElement = document.createElement("header");
 
-    // Add an ID to the header for styling or identification if needed
     headerElement.id = "header";
 
-    // Insert the header at the top of the <body>
     document.body.prepend(headerElement);
 
-    // Fetch the header content and populate the new <header> element
     fetch("components/header.html")
         .then((response) => response.text())
         .then((data) => {
-            headerElement.innerHTML = data; // Add content to the new header
+            headerElement.innerHTML = data; 
         })
         .catch((error) => console.error("Error loading header:", error));
 
-    // Add CSS links to the <head>
-    const headElement = document.head;
-
-    // List of links to be added
     const links = [
         {
             href: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css",
             rel: "stylesheet",
-            integrity: "sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH",
-            crossorigin: "anonymous"
+            integrity:
+                "sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH",
+            crossorigin: "anonymous",
         },
         { href: "css/all.min.css", rel: "stylesheet" },
-        { href: "css/fontawesome.min.css", rel: "stylesheet" }
+        { href: "css/fontawesome.min.css", rel: "stylesheet" },
     ];
 
-    // Dynamically add each link
     links.forEach(({ href, rel, integrity, crossorigin }) => {
         const linkElement = document.createElement("link");
         linkElement.href = href;
