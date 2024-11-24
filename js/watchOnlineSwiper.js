1805403264cd90248b9f7e3bfc9aa96c6d86a188
l4nzel0d <borisych095@yandex.ru>
2024-11-24 10:37:21 +0300

    fix: set slide-info-wrapper height explicitly, switch backdrop position to absolute; tweak: disable autplay

--- js/watchOnlineSwiper.js
+++ js/watchOnlineSwiper.js
@@ -1,10 +1,10 @@
 var swiper = new Swiper(".watch-online__swiper", {
     slidesPerView: 1,
     spaceBetween: -1,
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-    },
+    // autoplay: {
+    //     delay: 2500,
+    //     disableOnInteraction: false,
+    // },
     loop: true,
     // pagination: {
     //     el: ".swiper-pagination",
