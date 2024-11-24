65c83aed1a30d235baf52790787356e5c86c85c4
l4nzel0d <borisych095@yandex.ru>
2024-11-24 10:29:30 +0300

    feat: turn autoplay on swiper

--- js/watchOnlineSwiper.js
+++ js/watchOnlineSwiper.js
@@ -1,13 +1,13 @@
 var swiper = new Swiper(".watch-online__swiper", {
     slidesPerView: 1,
     spaceBetween: -1,
-    // autoplay: {
-    //     delay: 2500,
-    //     disableOnInteraction: false,
-    // },
-    loop: true,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
     },
+    loop: true,
+    // pagination: {
+    //     el: ".swiper-pagination",
+    //     clickable: true,
+    // },
 });
